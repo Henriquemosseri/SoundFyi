76a621aac1f7cebc03b1593837ee327ac3a4d012
Henrique Mosseri <henriquemosseri@gmail.com>
2024-02-12 17:30:21 -0300

    first commit

---
+++ src/br/com/alura/SoundFyi/Audio.java
@@ -0,0 +1,10 @@
+package br.com.alura.SoundFyi;
+
+public class Audio {
+    private String titulo;
+    private int duracao;
+    private int totalReproducoes;
+    private int curtidas;
+    private String Classificacoes;
+
+}
